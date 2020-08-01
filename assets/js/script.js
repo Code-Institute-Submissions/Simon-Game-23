d1ceb44456d4e219961101598ba6f97882c54194
Effull <56739030+Effull@users.noreply.github.com>
2020-08-01 17:09:59 +0100

    Add files via upload
    finally got the comparison to work and also developed on the counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-let turn = 0;
 let playerarr = [];
 let computerarr = [];
+let isOneLife = false;
+let isSuperSpeed = false;
+let round;
 
 const topRight = document.querySelector(".top-right-panel");
 const topLeft = document.querySelector(".top-left-panel");
 const bottomRight = document.querySelector(".bottom-left-panel");
 const bottomLeft = document.querySelector(".bottom-right-panel");
+const onelife = document.querySelector(".onelife");
+const superSpeed = document.querySelector(".superSpeed");
+const roundCounter = document.querySelector("#turn");
 
 function gamestart() {
     startButton.style.display = 'none';
@@ -49,18 +54,22 @@ if (chosenElement == topLeft) {
 
 
 function flashYellow(){
+ computerarr.push(topLeft);
  topLeft.style.backgroundColor = "white";
  setTimeout(function(){topLeft.style.backgroundColor = "Yellow";},500);
 }
 function flashGreen(){
+  computerarr.push(topRight);
  topRight.style.backgroundColor = "white";
  setTimeout(function(){topRight.style.backgroundColor = "Green";},500);
 }
 function flashBlue(){
+computerarr.push(bottomLeft);
  bottomRight.style.backgroundColor = "white";
  setTimeout(function(){bottomRight.style.backgroundColor = "Blue";},500);
 }
 function flashRed(){
+computerarr.push(bottomRight);
  bottomLeft.style.backgroundColor = "white";
  setTimeout(function(){bottomLeft.style.backgroundColor = "red";},500);
 }
@@ -97,27 +106,61 @@ function alertRed(){
 
 
 
-function computerarrVsPlayerarr(){
-if (JSON.stringify(playerarr) == JSON.stringify(computerarr)){
-  round + 1;
-  flahsComputerSequons()
-} else{
-  alert("Lose")
-}
+function computerarrVsPlayerarr() {
+    for (let i = 0; i < playerarr.length; ++i) {
+        if (playerarr[i] === computerarr[i]) {
+            // all good, continue
+
+            const thisTheLastItem = (i === computerarr.length - 1)
+            const hasThePlayerRepeatedTheWholeSequence = (computerarr.length === playerarr.length)
+            if (thisTheLastItem && hasThePlayerRepeatedTheWholeSequence) {
+                // set timeout so that button doesn't flash
+                // as soon as sequence is completely correct
+                setTimeout(flahsComputerSequons, 2 * 1000)
+            }
+        } else {
+            topLeft.style.backgroundColor = "white";
+            topRight.style.backgroundColor = "white";
+            bottomLeft.style.backgroundColor = "white";
+            bottomRight.style.backgroundColor = "white";
+            setTimeout(function() {
+                topLeft.style.backgroundColor = "Yellow";
+            }, 1000);
+            setTimeout(function() {
+                topRight.style.backgroundColor = "Green";
+            }, 1000);
+            setTimeout(function() {
+                bottomLeft.style.backgroundColor = "Blue";
+            }, 1000);
+            setTimeout(function() {
+                bottomRight.style.backgroundColor = "red";
+            }, 1000);
+        }
+    }
 }
 
-function flahsComputerSequons(){
-for (let i = 0; i < computerarr.length; ++i){
-  const item = computerarr[i];
-
-  flashButton(item)
+function flahsComputerSequons() {
+    turn++;
+    roundCounter.innerHTML = turn;
+    let i = 0
+    for (i = 0; i < computerarr.length; ++i) {
+        const item = computerarr[i];
+            setTimeout(function() {
+                flashButton(item)
+            }, i * 800)
+    }
+    setTimeout(newRound, i * 1000)
 }
-}
+
+function flashButton(buttonElement, delay) {
+    buttonElement.classList.add('flashing')
+        setTimeout(function() {
+            buttonElement.classList.remove('flashing')
+        }, 800)
+    }
 
 
 
-function flashButton(){
-  topLeft.style.background = 'white'
-  
-}
+
+console.log(playerarr);
 
