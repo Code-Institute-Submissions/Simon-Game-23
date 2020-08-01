b318e09ac6666fcd34686433af47c57c95cfc5a2
Effull <56739030+Effull@users.noreply.github.com>
2020-08-01 14:41:26 +0100

    Add files via upload
    having issues trying to push the result from the function to pick a random panel and then trying to flash the specific panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,41 @@ let computerarr = [];
 
 const topRight = document.querySelector(".top-right-panel");
 const topLeft = document.querySelector(".top-left-panel");
-const bottomRight = document.querySelector(".bottom-right-panel");
-const bottomLeft = document.querySelector(".bottom-left-panel");
-
-
-function gamestart(){
-  startButton.style.display = 'none';
-  bottomLeft.style.display = 'inline-block';
-  bottomRight.style.display = 'inline-block';
-  topRight.style.display = 'inline-block';
-  topLeft.style.display = 'inline-block';
-  setTimeout(function(){ flashColour(); }, 1000);
-};
-
+const bottomRight = document.querySelector(".bottom-left-panel");
+const bottomLeft = document.querySelector(".bottom-right-panel");
 function getRandomPanel(){
 const panels = [ topLeft, topRight, bottomLeft, bottomRight];
-return panels[Math.floor(Math.random() * panels.length)];
+return item = panels[Math.floor(Math.random() * panels.length)];
 }
-
-console.log(getRandomPanel())
-
-function flashedColuer(){
-  getRandomPanel.push(computerarr)
-};
-
-console.log(computerarr)
+const computerarr = [
+  getRandomPanel()
+]
+if (computerarr == topLeft) {
+  flashYellow();
+} else if (computerarr == "topRight") {
+  flashGreen();
+} else if (computerarr == "bottom-left-panel") {
+  flashBlue();
+} else if (computerarr == "div.pannel.bottom-right-panel") {
+  flashRed();
+} else {
+  console.log("error");
+}
+function flashYellow(){
+ topLeft.style.backgroundColor = "white";
+ setTimeout(function(){topLeft.style.backgroundColor = "Yellow";},500);
+}
+function flashGreen(){
+ topRight.style.backgroundColor = "white";
+ setTimeout(function(){topRight.style.backgroundColor = "Green";},500);
+}
+function flashBlue(){
+ bottomRight.style.backgroundColor = "white";
+ setTimeout(function(){bottomRight.style.backgroundColor = "Blue";},500);
+}
+function flashRed(){
+ bottomLeft.style.backgroundColor = "white";
+ setTimeout(function(){bottomLeft.style.backgroundColor = "red";},500);
+}
+console.log(computerarr);
 
