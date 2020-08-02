30f20fe37464d849b8fafb9f70ef55a9ab372f2e
Effull <56739030+Effull@users.noreply.github.com>
2020-08-02 11:31:36 +0100

    Add files via upload
    started to implement superspeed and one life

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,28 @@ let round;
 
 const topRight = document.querySelector(".top-right-panel");
 const topLeft = document.querySelector(".top-left-panel");
-const bottomRight = document.querySelector(".bottom-left-panel");
-const bottomLeft = document.querySelector(".bottom-right-panel");
+const bottomRight = document.querySelector(".bottom-right-panel");
+const bottomLeft = document.querySelector(".bottom-left-panel");
 const onelife = document.querySelector(".onelife");
 const superSpeed = document.querySelector(".superSpeed");
 const roundCounter = document.querySelector("#turn");
+
+
+onelife.addEventListener("click", (event) => {
+    if (onelife.checked == true) {
+        isOneLife = true
+    } else {
+        isOneLife = false;
+    }
+})
+
+superSpeed.addEventListener("click", (event) => {
+    if (superSpeed.checked == true) {
+        isOneLife = true
+    } else {
+        superSpeed = false;
+    }
+})
 
 function gamestart() {
     startButton.style.display = 'none';
@@ -19,91 +36,181 @@ function gamestart() {
     topRight.style.display = 'inline-block';
     topLeft.style.display = 'inline-block';
     turn = 1;
+    roundCounter.innerHTML = 1;
     setTimeout(function() {
         newRound();
     }, 1000);
 };
 
+function getRandomPanel() {
+    const panels = [topLeft, topRight, bottomLeft, bottomRight];
+    return panels[Math.floor(Math.random() * panels.length)];
+}
+
+console.log(getRandomPanel())
 
 function newRound() {
     playerarr = []
     flashColour()
 }
 
-function getRandomPanel(){
-const panels = [ topLeft, topRight, bottomLeft, bottomRight];
-return item = panels[Math.floor(Math.random() * panels.length)];
+// This is the function that
+// will cause a new color to be added to
+// the computerarray and for that color
+// to be flashed.
+function flashColour() {
+    const chosenElement = getRandomPanel();
+    if (chosenElement == topLeft) {
+        isComputerPlayingSequence = true;
+        flash(topLeft)
+    } else if (chosenElement == topRight) {
+        isComputerPlayingSequence = true;
+        flashGreen()
+    } else if (chosenElement == bottomLeft) {
+        isComputerPlayingSequence = true;
+        flashBlue()
+    } else {
+        isComputerPlayingSequence = true;
+        flashRed()
+    }
 }
 
-function flashColour(){
-const chosenElement = getRandomPanel();
-if (chosenElement == topLeft) {
-  isComputerPlayingSequence = true;
-  flashYellow()
-} else if (chosenElement == topRight) {
-  isComputerPlayingSequence = true;
-  flashGreen()
-} else if (chosenElement == bottomLeft) {
-  isComputerPlayingSequence = true;
-  flashBlue()
-} else {
-  isComputerPlayingSequence = true;
-  flashRed()
-}
+
+function flash(element) {
+  
+    let originalColor = element.style.background;
+    element.style.background = 'white';
+    // wait here
+    element.style.background = originalColor;
 }
 
 
-function flashYellow(){
- computerarr.push(topLeft);
- topLeft.style.backgroundColor = "white";
- setTimeout(function(){topLeft.style.backgroundColor = "Yellow";},500);
-}
-function flashGreen(){
-  computerarr.push(topRight);
- topRight.style.backgroundColor = "white";
- setTimeout(function(){topRight.style.backgroundColor = "Green";},500);
-}
-function flashBlue(){
-computerarr.push(bottomLeft);
- bottomRight.style.backgroundColor = "white";
- setTimeout(function(){bottomRight.style.backgroundColor = "Blue";},500);
-}
-function flashRed(){
-computerarr.push(bottomRight);
- bottomLeft.style.backgroundColor = "white";
- setTimeout(function(){bottomLeft.style.backgroundColor = "red";},500);
-}
+
+
+
+
 console.log(computerarr);
 
-function alertYellow(){
-  playerarr.push(topLeft)
-  topLeft.style.backgroundColor = "white";
-  setTimeout(function(){topLeft.style.backgroundColor = "Yellow";},400);
-  computerarrVsPlayerarr()
+function flashYellow() {
+    computerarr.push(topLeft);
+    topLeft.style.backgroundColor = "white";
+    setTimeout(function() {
+        isComputerPlayingSequence = false;
+    }, 400);
+    if (superSpeed == true) {
+        setTimeout(function() {
+            topLeft.style.backgroundColor = "Yellow";
+        }, 400);
+    } else {
+        setTimeout(function() {
+            topLeft.style.backgroundColor = "Yellow";
+        }, 800);
+    };
 };
 
-function alertGreen(){
-  playerarr.push(topRight)
-  topRight.style.backgroundColor = "white";
-  setTimeout(function(){topRight.style.backgroundColor = "Green";},400);
-  computerarrVsPlayerarr()
+function flashGreen() {
+    computerarr.push(topRight);
+    topRight.style.backgroundColor = "white";
+    setTimeout(function() {
+        isComputerPlayingSequence = false;
+    }, 400);
+    if (superSpeed == true) {
+        setTimeout(function() {
+            topRight.style.backgroundColor = "Green";
+        }, 400);
+    } else {
+        setTimeout(function() {
+            topRight.style.backgroundColor = "Green";
+        }, 800);
+    };
 };
 
-function alertBlue(){
-  playerarr.push(bottomLeft)
-  bottomLeft.style.backgroundColor = "white";
-  setTimeout(function(){bottomLeft.style.backgroundColor = "Blue";},400);
-  computerarrVsPlayerarr()
+function flashBlue() {
+    computerarr.push(bottomLeft);
+    bottomLeft.style.backgroundColor = "white";
+    setTimeout(function() {
+        isComputerPlayingSequence = false;
+    }, 400);
+    if (superSpeed == true) {
+        setTimeout(function() {
+            bottomLeft.style.backgroundColor = "Blue";
+        }, 400);
+    } else {
+        setTimeout(function() {
+            bottomLeft.style.backgroundColor = "Blue";
+        }, 800);
+    };
 };
 
-function alertRed(){
-  playerarr.push(bottomRight)
-  bottomRight.style.backgroundColor = "white";
-  setTimeout(function(){bottomRight.style.backgroundColor = "red";},400);
-  computerarrVsPlayerarr()
+function flashRed() {
+    computerarr.push(bottomRight);
+    bottomRight.style.backgroundColor = "white";
+    setTimeout(function() {
+        isComputerPlayingSequence = false;
+    }, 400);
+    if (superSpeed == true) {
+        setTimeout(function() {
+            bottomRight.style.backgroundColor = "red";
+        }, 800);
+    } else {
+        setTimeout(function() {
+            bottomRight.style.backgroundColor = "red";
+        }, 400);
+    };
 };
 
 
+function alertYellow() {
+    if (isComputerPlayingSequence ^= true) {
+        return
+    } else {
+        playerarr.push(topLeft)
+        topLeft.style.backgroundColor = "white";
+        setTimeout(function() {
+            topLeft.style.backgroundColor = "Yellow";
+        }, 400);
+        computerarrVsPlayerarr()
+    };
+};
+
+function alertGreen() {
+    if (isComputerPlayingSequence ^= true) {
+        return
+    } else {
+        playerarr.push(topRight)
+        topRight.style.backgroundColor = "white";
+        setTimeout(function() {
+            topRight.style.backgroundColor = "Green";
+        }, 400);
+        computerarrVsPlayerarr()
+    };
+};
+
+function alertBlue() {
+    if (isComputerPlayingSequence ^= true) {
+        return
+    } else {
+        playerarr.push(bottomLeft)
+        bottomLeft.style.backgroundColor = "white";
+        setTimeout(function() {
+            bottomLeft.style.backgroundColor = "Blue";
+        }, 400);
+        computerarrVsPlayerarr()
+    };
+};
+
+function alertRed() {
+    if (isComputerPlayingSequence ^= true) {
+        return
+    } else {
+        playerarr.push(bottomRight)
+        bottomRight.style.backgroundColor = "white";
+        setTimeout(function() {
+            bottomRight.style.backgroundColor = "red";
+        }, 400);
+        computerarrVsPlayerarr()
+    };
+};
 
 
 function computerarrVsPlayerarr() {
@@ -135,6 +242,21 @@ function computerarrVsPlayerarr() {
             setTimeout(function() {
                 bottomRight.style.backgroundColor = "red";
             }, 1000);
+            setTimeout(function() {
+                if (onelife == true) {
+                    computerarr = [];
+                    newRound();
+                    round = 0;
+                } else {
+                    let i = 0
+                    for (i = 0; i < computerarr.length; ++i) {
+                        const item = computerarr[i];
+                        setTimeout(function() {
+                            flashButton(item)
+                        }, i * 1000)
+                    }
+                }
+            }, 1500);
         }
     }
 }
@@ -145,22 +267,32 @@ function flahsComputerSequons() {
     let i = 0
     for (i = 0; i < computerarr.length; ++i) {
         const item = computerarr[i];
+        if (superSpeed) {
+            setTimeout(function() {
+                flashButton(item)
+            }, i * 400)
+        } else {
             setTimeout(function() {
                 flashButton(item)
             }, i * 800)
+        }
     }
     setTimeout(newRound, i * 1000)
 }
 
 function flashButton(buttonElement, delay) {
     buttonElement.classList.add('flashing')
+    if (superSpeed) {
+        setTimeout(function() {
+            buttonElement.classList.remove('flashing')
+        }, 400)
+    } else {
         setTimeout(function() {
             buttonElement.classList.remove('flashing')
         }, 800)
     }
-
+}
 
 
 
 console.log(playerarr);
-
